bb011e186bebfab75919d4de6da58b8ca34a1519
ZulZayn <zulwaqarzain96@gmail.com>
2022-03-10 16:29:43 +0800

    API: Forgot Password

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -80,5 +80,53 @@ class AuthController extends Controller
             'token' => $request->authToken
         ]);
     }
+
+    public function validateUser(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255',
+            'phone' => 'required|string|max:255',
+        ]);
+
+        $user = User::where('name' , $request->name)->where('email' , $request->email)->where('phone' , $request->phone)->first();
+        
+        if($user){
+            return response([
+                'status' => 'success',
+                'user' => $user,
+            ]);
+        }
+        else{
+            return response([
+                'status' => 'fail',
+                'user' => '',
+            ]);
+        }
+    }
+
+
+    public function changeForgotPassword(Request $request){
+
+        $request->validate([
+            'user_id' => 'required',
+            'password' => ['required', 'confirmed'],
+        ]);
+
+        $user = User::find($request->user_id); 
+        
+
+
+            $user->password = Hash::make($request->password);
+
+            $user->save();
+
+            return response([
+                'status' => 'success'
+            ]);
+
+       
+
+    }
    
 }
