94dcc48ba61242eaa773d5e614a64cec9d451729
SyazaitulMNR <syazaitulmnr@gmail.com>
2022-03-10 17:25:08 +0800

    100322_loginAdmin

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -33,8 +35,26 @@ class LoginController extends Controller
      *
      * @return void
      */
-    public function __construct()
-    {
-        $this->middleware('adminLogin')->except('logout');
+
+    public function login(Request $request)
+    {   
+        $input = $request->all();
+   
+        $this->validate($request, [
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+   
+        if(auth()->attempt(array('email' => $input['email'], 'password' => $input['password'])))
+        {
+            if (auth()->user()->role == 'user') {
+                return redirect()->route('login')->with('error','Access denied.');
+            }else{
+                return redirect()->route('home');
+            }
+        }else{
+            return redirect()->route('login')->with('error','Email-Address And Password Are Wrong.');
+        }
+          
     }
 }
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,64 +10,67 @@
 @section('contentGuest')
     <div class="content">
         <div class="container">
-        <div class="col-md-12 ml-auto mr-auto">
-            <div class="header bg-gradient-primary py-10 py-lg-2 pt-lg-12">
-                <div class="container">
-                    <div class="header-body text-center mb-7">
-                        <div class="row justify-content-center">
-                            <div class="col-lg-12 col-md-9">
-                                <p class="text-lead text-light mt-3 mb-0">
-                                    @include('alerts.migrations_check')
-                                </p>
-                            </div>
-                            <div class="col-lg-5 col-md-6">
+            <div class="col-md-12 ml-auto mr-auto">
+                <div class="header bg-gradient-primary py-10 py-lg-2 pt-lg-12">
+                    <div class="container">
+                        <div class="header-body text-center mb-7">
+                            <div class="row justify-content-center">
+                                <div class="col-lg-12 col-md-9">
+                                    <p class="text-lead text-light mt-3 mb-0">
+                                        @include('alerts.migrations_check')
+                                    </p>
+                                </div>
+                                <div class="col-lg-5 col-md-6">
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="col-md-4 ml-auto mr-auto">
-            <form role="form" method="POST" action="{{ route('login') }}">
+
+            <div class="col-md-4 ml-auto mr-auto">
+                <form role="form" method="POST" action="{{ route('login') }}">
                 @csrf
-            <div class="card card-login card-plain">
-                <div class="card-header text-center">
-                    <img src="{{ asset('assets/img/icon_1.png') }}" style="max-width:50%" alt="">
-                </div>
-                <div class="card-body ">
-                <div class="input-group no-border form-control-lg {{ $errors->has('email') ? ' has-danger' : '' }}">
-                    <span class="input-group-prepend">
-                    <div class="input-group-text">
-                        <i class="now-ui-icons users_circle-08"></i>
+                    <div class="card card-login card-plain">
+                        <div class="card-header text-center pb-3">
+                            <img src="{{ asset('assets/img/icon_1.png') }}" style="max-width:50%" alt="">
+                        </div>
+
+                        @if(session('error'))          
+                            <div class="alert alert-danger">
+                                <button type="button" aria-hidden="true" class="close">
+                                    <i class="now-ui-icons ui-1_simple-remove"></i>
+                                </button>
+                                <small>{{ session('error') }}</small>
+                            </div>
+                        @endif
+
+                        <div class="card-body ">
+                            <div class="input-group no-border form-control-lg">
+                                <span class="input-group-prepend">
+                                <div class="input-group-text">
+                                    <i class="now-ui-icons users_circle-08"></i>
+                                </div>
+                                </span>
+                                <input class="form-control" placeholder="Please Enter Email" type="email" name="email" required autofocus>
+                            </div>
+
+                            <div class="input-group no-border form-control-lg">
+                                <div class="input-group-prepend">
+                                <div class="input-group-text">
+                                    <i class="now-ui-icons objects_key-25"></i></i>
+                                </div>
+                                </div>
+                                <input class="form-control" name="password" placeholder="Please Enter Password" type="password" required>
+                            </div>
+                    
+                        </div>
+                        <div class="card-footer">
+                            <button  type = "submit" class="btn btn-primary btn-round btn-lg btn-block mb-3">{{ __('Get Started') }}</button>
+                        </div>
                     </div>
-                    </span>
-                    <input class="form-control {{ $errors->has('email') ? ' is-invalid' : '' }}" placeholder="Please Enter Email" type="email" name="email" required autofocus>
-                </div>
-                @if ($errors->has('email'))
-                    <span class="invalid-feedback" style="display: block;" role="alert">
-                    <strong>{{ $errors->first('email') }}</strong>
-                    </span>
-                @endif
-                <div class="input-group no-border form-control-lg {{ $errors->has('password') ? ' has-danger' : '' }}">
-                    <div class="input-group-prepend">
-                    <div class="input-group-text">
-                        <i class="now-ui-icons objects_key-25"></i></i>
-                    </div>
-                    </div>
-                    <input class="form-control {{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" placeholder="Please Enter Password" type="password" required>
-                </div>
-                @if ($errors->has('password'))
-                    <span class="invalid-feedback" style="display: block;" role="alert">
-                    <strong>{{ $errors->first('password') }}</strong>
-                    </span>
-                @endif
-                </div>
-                <div class="card-footer ">
-                    <button  type = "submit" class="btn btn-primary btn-round btn-lg btn-block mb-3">{{ __('Get Started') }}</button>
-                </div>
+                </form>
             </div>
-            </form>
-        </div>
         </div>
     </div>
 @endsection
