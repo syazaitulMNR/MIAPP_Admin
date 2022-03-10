14f0970c63008f4ae6756da261ba7ba77200eeba
ZulZayn <zulwaqarzain96@gmail.com>
2022-03-10 23:46:51 +0800

    Program Ratio

--- resources/views/pages/programs/create.blade.php
+++ resources/views/pages/programs/create.blade.php
@@ -152,7 +152,7 @@
   
         cropper = new Cropper(image, {
           dragMode: 'move',
-          aspectRatio: 2/1,
+          aspectRatio: 2.1/1,
           autoCropArea: 0.65,
           restore: false,
           guides: false,
